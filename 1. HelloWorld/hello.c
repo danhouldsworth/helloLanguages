002782ca2489ac7d3f177bcc79880a0f170a577e
Dan Houldsworth <dan@bramble.io>
2015-01-08 19:33:59 +0000

    Update

--- 1. HelloWorld/hello.c
+++ 1. HelloWorld/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>                  // <libraries> looked within compilation include path vs "libraries" absolute path
 
-int main(int argc, char *argv[])    // main() is expected to return an int, and (I think) takes args as number, and
+int main(int argc, char *argv[])    // main() is expected to return an int, and (I think) takes args as number, and pointer to array of chars. Need to check if this is command line args...
 {
     puts("Hello, world!");
     return 0;
--- 3. Iterations/iterations.js
+++ 3. Iterations/iterations.js
@@ -11,6 +11,30 @@ var fibb = function(x){
     else return fibb(x - 1) + fibb(x - 2);
 };
 
+var pi = function(dp){
+    if (dp === 0) return 3; // No need for else as already branched
+
+    function leibnizTerm (n){
+        return Math.pow(-1, n) / (n + n + 1);
+    }
+    function truncate (x, precision) {
+        return parseInt(x * Math.pow(10,precision) ) / Math.pow(10,precision); // Does not round
+    }
+    function round(decimal){
+        return (decimal + 0.5) | 0; // Fast round by bitwise OR
+    }
+
+    var sum = 0.0;
+    var n = 0;
+
+    while ( truncate(sum, dp - 1) !== pi(dp - 1) ) {
+        // console.log(truncate(sum, dp - 1) + " vs " + truncate(lastSum, dp));
+        sum += 4 * leibnizTerm(n++);
+    }
+
+    return truncate(sum, dp);
+}
+
 for (i = 0; i <= n; i++){
-    console.log(i + "! = " + factorial(i) + ",  \t" + i + "th Fibonacci = " + fibb(i) );
+    console.log(i + "! = " + factorial(i) + ",  \t" + i + "th Fibonacci = " + fibb(i) + ",   \t PI=" + pi(i) );
 }
