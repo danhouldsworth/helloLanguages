945fcd0edb2e980ec6415007611fa44ae40200b5
Dan Houldsworth <dan@bramble.io>
2015-01-10 20:29:09 +0000

    Nailed C strings!

--- 1. HelloWorld/hello.c
+++ 1. HelloWorld/hello.c
@@ -6,12 +6,12 @@
 int main(int argc, char *argv[])    // main() is expected to return an int, and (I think) takes args as number, and pointer to array of chars. Need to check if this is command line args...
 {
     char greeting1[] = "Hello";
-    char greeting2[] = {44, 32, 0}; // chars are ints !!
+    char greeting2[] = {44, 32, 0}; // chars are unsigned ints !!
     char greeting3[] = {'w','o','r','l','d','!','\0'}; // strings are terminated by 'null' (the 0 byte)
     printf("%s%s%s\n", greeting1, greeting2, greeting3);
     return 0;
 }
 
-// Learning : While C can be a pain with strings, I love the way to forces you to think about the memory representation from the outset
+// Learning : Although C can be a pain with strings, I do love the way to forces you to think about the memory representation from the outset
 
 
--- 2. Strings/strings.c
+++ 2. Strings/strings.c
@@ -1,29 +1,54 @@
 // 1. Done (Ye-hah!)
-
+// 2. Done
+// 3. Done
+// 4. Bonus. Forwards is fine. Backwards and partial give an insight into how unicode is stored and displayed. Am guessing it is 3xbytes
 
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
 
-    char greeting[] = "Hello, world!";
-    char copy[0]; // Or could do copy[] = ''
+    // char greeting[] = "Hello, world!";
+    char greeting[] = "The quick brown 狐 jumped over the lazy 犬";
+    char *ptr = greeting; // pointer to this string
+    int index = 0;
 
-    char *i = greeting; // pointers to the strings
-
-    int greetIndex = 0;
-    int copyIndex = 0;
-
-    while (*i++) { // While the memory content pointed to by the pointer i is non-zero, count the greeting length
-        greetIndex++;
+    while (*ptr++) { // Note increment operator (++/--) takes precedence over indirect reference operator (*) : equiv *(ptr++)
+        index++;
     };
 
-    while (greetIndex--) {
-        copy[copyIndex++] = greeting[greetIndex];
+    while (index--) { // Don't forget, this condition still gets expressed even if the while doesn't get executed. ==> index will end up at -1 not 0
+        putchar(greeting[index]);
     };
 
-    copy[copyIndex] = 0; // Terminate the new string
+    putchar('\n');
+    ptr = greeting;
 
-    puts(copy);
+    while (*ptr) {
+        if ((*ptr >= 65) && (*ptr <=90)) putchar(*ptr + 32); // ASCII 65-90 A-Z, 97-122 a-z
+        else putchar(*ptr);
+        ptr++;
+    }
+
+    putchar('\n');
+    ptr = greeting;
+
+    while (*ptr) {
+        if ((*ptr >= 97) && (*ptr <=122)) putchar(*ptr - 32);
+        else putchar(*ptr);
+        ptr++;
+    }
+
+    putchar('\n');
+    ptr = greeting;
+    index = 0;
+
+    while (*ptr++) {
+        for (int i = 0; i <= index; i++) {
+            putchar(greeting[i]);
+        }
+        putchar('\n');
+        index++;
+    }
 }
 
 // Learnings
