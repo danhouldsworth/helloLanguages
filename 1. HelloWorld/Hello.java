b462641c52598f145298770ef1e25bf7786cfec7
Alec Hewitt <alec.hewitt9@gmail.com>
2015-01-11 18:23:27 +0000

    Rename Public class name 'hello' -> 'Hello' in order to keep with capitlization conventions of public classes in Java

--- 1. HelloWorld/Hello.java
+++ 1. HelloWorld/Hello.java
@@ -1,7 +1,7 @@
 // 1. Done
 // Bonus. Not yet...
 
-public class hello {        // Note : public classes need the same name as filename
+public class Hello {        // Note : public classes need the same name as filename
    public static void main(String[] args) {
       System.out.println("Hello, World!");
    }
