4ba0391b4f385241d558557dd02d441b6a68da85
Dan Houldsworth <dan@bramble.io>
2015-01-13 16:09:03 +0000

    Added quit command to telnet

--- 6.2 TelnetServer/telnet.go
+++ 6.2 TelnetServer/telnet.go
@@ -1,10 +1,10 @@
 // 1. Done
 // 2. Done (for sinlge digits 0-4 !)
+// 3. Done
 
 package main
 
 import (
-	// "fmt"
 	"net"
 )
 
@@ -20,10 +20,11 @@ func handleConnection(c net.Conn) {
 	welcome := []byte("\n\n --- Welcome to Telnet Echo & Double in Go! --- \n\n")
 	echo := []byte("Server echo service : ")
 	double := []byte("Server doubling service : ")
-	messageBuffer := make([]byte, 1024)
+	bye := []byte("\n\n --- Bye for now!! --- \n\n")
 
 	c.Write(welcome)
 	for {
+		messageBuffer := make([]byte, 1024)
 		c.Read(messageBuffer)
 		if messageBuffer[0] >= 48 && messageBuffer[0] <= 57 {
 			messageBuffer[0] -= 48
@@ -31,6 +32,9 @@ func handleConnection(c net.Conn) {
 			messageBuffer[0] += 48
 			c.Write(double)
 			c.Write(messageBuffer)
+		} else if messageBuffer[0] == 'q' && messageBuffer[1] == 'u' && messageBuffer[2] == 'i' && messageBuffer[3] == 't' {
+			c.Write(bye)
+			c.Close()
 		} else {
 			c.Write(echo)
 			c.Write(messageBuffer)
