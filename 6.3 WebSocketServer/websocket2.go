ee3a8665c697cdd3fdc7425f55d991b43764948b
Alec Hewitt <alec.hewitt9@gmail.com>
2015-01-20 10:28:17 +0000

    Second websokets in go, sending messages broadcasting messages to all clients @

---
+++ 6.3 WebSocketServer/websocket2.go
@@ -0,0 +1,62 @@
+// 1. Done
+
+// To run must set $GOPATH
+// and either run: go install
+// or: go get "github.com/gorilla/websocket"
+// for more details on packages see here: http://golang.org/doc/code.html
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var connections map[*websocket.Conn]bool
+
+func sendAll(msg []byte) {
+	for conn := range connections {
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			delete(connections, conn)
+			conn.Close()
+		}
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Upgrade http connection to websocket connection
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Add this connection to the list of connections
+	connections[conn] = true
+
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			delete(connections, conn)
+			conn.Close()
+			return
+		}
+
+		// Print the message they have just sent to us
+		log.Println(string(msg))
+
+		// Print the message type, todo: not working well
+		log.Println(string(messageType))
+
+		// Send message to all other connected clients
+		sendAll(msg)
+	}
+}
+
+func main() {
+	connections = make(map[*websocket.Conn]bool)
+	
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	http.HandleFunc("/ws", wsHandler)
+	http.ListenAndServe(":8080", nil)
+}
