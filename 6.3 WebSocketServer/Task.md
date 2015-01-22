b9c9a085fc11637a20a79abdcab3090d9cc4ddd1
Alec Hewitt <alec.hewitt9@gmail.com>
2015-01-22 21:03:36 +0000

    tidying up of websoket files, update task and small update to index.html@

--- 6.3 WebSocketServer/Task.md
+++ 6.3 WebSocketServer/Task.md
@@ -1,7 +1,7 @@
 #Task
 
 1. Accept a single WebSocket client connection, provide an echo response for data recieved.
-2. Accept two client connections, providing a relay between the two.
-3. Accept unlimited number of connections, broadcasting all incoming messages to all others.
+2. Accept unlimited number of connections, broadcasting all incoming messages to all others.
+3. Accept two client connections per room, providing a relay between the two.
 
 Bonus : Establish a message protocol of {'command', 'message'}, where command can either be sendToID, or sendToAll.
--- 6.3 WebSocketServer/websocket2.go
+++ 6.3 WebSocketServer/websocket2.go
@@ -1,62 +1,59 @@
-// 1. Done
+// 2. Done
 
-// To run must set $GOPATH
-// and either run: go install
-// or: go get "github.com/gorilla/websocket"
-// for more details on packages see here: http://golang.org/doc/code.html
 package main
 
 import (
-	"github.com/gorilla/websocket"
-	"log"
-	"net/http"
+    "github.com/gorilla/websocket"
+    "log"
+    "net/http"
 )
 
 var connections map[*websocket.Conn]bool
 
 func sendAll(msg []byte) {
-	for conn := range connections {
-		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			delete(connections, conn)
-			conn.Close()
-		}
-	}
+    for conn := range connections {
+        if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+            delete(connections, conn)
+            conn.Close()
+        }
+    }
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	// Upgrade http connection to websocket connection
-	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+    // Upgrade http connection to websocket connection
+    conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+    if err != nil {
+        log.Println(err)
+        return
+    }
 
-	// Add this connection to the list of connections
-	connections[conn] = true
+    // Add this connection to the list of connections
+    connections[conn] = true
+    log.Println(connections)
 
-	for {
-		messageType, msg, err := conn.ReadMessage()
-		if err != nil {
-			delete(connections, conn)
-			conn.Close()
-			return
-		}
+    for {
+        messageType, msg, err := conn.ReadMessage()
+        if err != nil {
+            delete(connections, conn)
+            conn.Close()
+            return
+        }
 
-		// Print the message they have just sent to us
-		log.Println(string(msg))
+        // Print the message they have just sent to us
+        log.Println(string(msg))
 
-		// Print the message type, todo: not working well
-		log.Println(string(messageType))
+        // Print the message type, todo: not working well
+        log.Println(string(messageType))
 
-		// Send message to all other connected clients
-		sendAll(msg)
-	}
+        // Send message to all other connected clients
+        sendAll(msg)
+    }
 }
 
 func main() {
-	connections = make(map[*websocket.Conn]bool)
-	
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
-	http.HandleFunc("/ws", wsHandler)
-	http.ListenAndServe(":8080", nil)
+    connections = make(map[*websocket.Conn]bool)
+
+    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+    http.HandleFunc("/ws", wsHandler)
+    http.ListenAndServe(":8080", nil)
 }
