8b64f5cc4221ed116492851f58f3d24dc6e58b81
Dan Houldsworth <dan@bramble.io>
2015-02-02 17:28:17 +0000

    Minor corrections to matching string and code

--- 01.1 Quine/quine.c
+++ 01.1 Quine/quine.c
@@ -14,11 +14,11 @@
 
 #define SUPERSPECIAL 64
 
-char programString[]="_#include <stdio.h>__#define COLON 58_#define UNDERSCORE 95_#define TAB 9_#define NEWLINE 10__#define DOUBLEQUOTE 34_#define SEMICOLON 59__#define SUPERSPECIAL 64__char programString[]=@__void parseForColonOrNewLine(int a){_:if(a == COLON) a = TAB;_:if(a == UNDERSCORE) a = NEWLINE;_:putchar(a);_}__int main(){_:int i = 0, j = 0;_:char symbol;_:while ((symbol = programString[i++]) != SUPERSPECIAL) parseForColonOrNewLine(symbol);_:putchar(DOUBLEQUOTE);_:while ((symbol = programString[j++]) != STRINGTERMINATOR) putchar(symbol);_:putchar(DOUBLEQUOTE);_:putchar(SEMICOLON);_:while ((symbol = programString[++i]) != STRINGTERMINATOR) parseForColonOrNewLine(symbol);_:return 0;_}__";
+char programString[]="_#include <stdio.h>__#define COLON 58_#define UNDERSCORE 95_#define TAB 9_#define NEWLINE 10__#define DOUBLEQUOTE 34_#define SEMICOLON 59__#define SUPERSPECIAL 64__char programString[]=@__void parseForColonOrNewLine(int a){_:if (a == COLON) a = TAB;_:if (a == UNDERSCORE) a = NEWLINE;_:putchar(a);_}__int main(){_:int i = 0, j = 0;_:char symbol;_:while ((symbol = programString[i++]) != SUPERSPECIAL) parseForColonOrNewLine(symbol);_:putchar(DOUBLEQUOTE);_:while ((symbol = programString[j++]) != STRINGTERMINATOR) putchar(symbol);_:putchar(DOUBLEQUOTE);_:putchar(SEMICOLON);_:while ((symbol = programString[i++]) != STRINGTERMINATOR) parseForColonOrNewLine(symbol);_:return 0;_}__";
 
 void parseForColonOrNewLine(char c){
-    if(c == COLON) c = TAB;
-    if(c == UNDERSCORE) c = NEWLINE;
+    if (c == COLON) c = TAB;
+    if (c == UNDERSCORE) c = NEWLINE;
     putchar(c);
 }
 
--- 01.1 Quine/quineBonus.c
+++ 01.1 Quine/quineBonus.c
@@ -1,5 +1,8 @@
-// Bonus. Done in 366 bytes (if you don't mind a compiler warning) :-)
-char d[]="char d[]=_void c(int a){_if(a==95)a=10;_putchar(a);_}_int main(){_int i=0,j=0;_while(i<9)c(d[i++]);_c(34);_while(putchar(d[j++])>0);_c(34);_c(59);_while(i<191)c(d[i++]);_return 0;_}_";
+// Bonus. Done in 365 bytes
+// Note : doesn't use superspecial character but instead hard-codes the position of the string.
+// Note : Gives a compiler warning for implicit use of stdio.h
+
+char d[]="char d[]=_void c(int a){_if(a==95)a=10;_putchar(a);_}_int main(){_int i=0,j=0;_while(i<9)c(d[i++]);_c(34);_while(putchar(d[j++])>0);_c(34);_c(59);_while(i<191)c(d[i++]);_return 0;_}";
 void c(int a){
 if(a==95)a=10;
 putchar(a);
