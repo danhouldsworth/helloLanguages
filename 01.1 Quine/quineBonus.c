8947bb798299bb35d21d2e5f6371e583c1467fe4
Dan Houldsworth <dan@bramble.io>
2015-02-02 19:15:57 +0000

    Added pointer solution, refined bonus to 341 bytes

--- 01.1 Quine/quineBonus.c
+++ 01.1 Quine/quineBonus.c
@@ -1,19 +1,7 @@
-// Bonus. Done in 365 bytes
-// Note : doesn't use superspecial character but instead hard-codes the position of the string.
-// Note : Gives a compiler warning for implicit use of stdio.h
+// Bonus. Done in 341 bytes
+// Note : doesn't use superspecial character but instead hard-codes the position of the string at 9-191
+// Note : Gives compiler warnings
 
-char d[]="char d[]=_void c(int a){_if(a==95)a=10;_putchar(a);_}_int main(){_int i=0,j=0;_while(i<9)c(d[i++]);_c(34);_while(putchar(d[j++])>0);_c(34);_c(59);_while(i<191)c(d[i++]);_return 0;_}";
-void c(int a){
-if(a==95)a=10;
-putchar(a);
-}
-int main(){
-int i=0,j=0;
-while(i<9)c(d[i++]);
-c(34);
-while(putchar(d[j++])>0);
-c(34);
-c(59);
-while(i<191)c(d[i++]);
-return 0;
-}
+char d[]="char d[]=_void c(int a){if(a==95)a=10;putchar(a);}_int main(){int i=0,j=0;while(i<9)c(d[i++]);c(34);while(putchar(d[j++])>0);c(34);c(59);while(i<191)c(d[i++]);return 0;}";
+void c(int a){if(a==95)a=10;putchar(a);}
+int main(){int i=0,j=0;while(i<9)c(d[i++]);c(34);while(putchar(d[j++])>0);c(34);c(59);while(i<191)c(d[i++]);return 0;}
